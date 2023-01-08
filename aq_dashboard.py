b32e24e5443743d508be88f453c3a38ea49f4818
Charles Black <charlieblk9400@gmail.com>
2023-01-08 11:11:53 -0800

    final submitted

--- aq_dashboard.py
+++ aq_dashboard.py
@@ -1,3 +1,4 @@
+# pylint: disable=too-few-public-methods
 """OpenAQ Air Quality Dashboard with Flask."""
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -5,9 +6,8 @@ import openaq
 
 app = Flask(__name__)
 
-import openaq
-
 def get_results():
+    '''create connection to openaq'''
     api = openaq.OpenAQ()
     status, body = api.measurements(city='Los Angeles', parameter='pm25')
     if status != 200:
@@ -19,11 +19,12 @@ def get_results():
         date = measurement['date']['utc']
         value = measurement['value']
         results.append((date, value))
-    
+
     return results
 
 @app.route('/')
 def root():
+    '''what displays in the home route'''
     records = Record.query.filter(Record.value >= 18).all()
     results = [(record.datetime, record.value) for record in records]
     return str(results)
@@ -34,10 +35,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 DB = SQLAlchemy(app)
 
 class Record(DB.Model):
+    '''create schema for datatable'''
     id = DB.Column(DB.Integer, primary_key=True)
     datetime = DB.Column(DB.String)
     value = DB.Column(DB.Float, nullable=False)
-  
+
 
     def __repr__(self):
         return f'Record(datetime={self.datetime}, value={self.value})'
@@ -47,7 +49,7 @@ def refresh():
     """Pull fresh data from Open AQ and replace existing data."""
     DB.drop_all()
     DB.create_all()
-    
+
     results = get_results()
     for result in results:
         date, value = result
@@ -56,57 +58,3 @@ def refresh():
 
     DB.session.commit()
     return 'Data refreshed!'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# from flask import Flask
-# import json
-# import requests
-# import openaq
-
-# app = Flask(__name__)
-
-# api = openaq.OpenAQ() 
-
-# @app.route('/')
-# body = api.cities()
-# print(body)
-
-
-# def root():
-#     '''call list of tuples and convert it to a sting and then return that'''
-#     result_string = str(list_of_tuples())
-#     return result_string
-
-# def list_of_tuples():
-#     '''create a list of tuples including just data and value from dataset'''
-#     response = requests.get('https://api.openaq.org/v1/measurements?city=Los Angeles&parameter=pm25')
-#     tuples_list = [(item['date']['utc'], item['value']) for item in response]
-#     return tuples_list
-
-
-
-
-
-
-
