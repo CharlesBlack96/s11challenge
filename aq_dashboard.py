87b89310fca2609de4d8380ad77abd5c7f2c3f3d
Charles Black <charlieblk9400@gmail.com>
2023-01-10 06:02:23 -0800

    final working submitted

--- aq_dashboard.py
+++ aq_dashboard.py
@@ -2,47 +2,25 @@
 """OpenAQ Air Quality Dashboard with Flask."""
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import openaq
+from openaq import OpenAQ
+
+api = OpenAQ()
 
 app = Flask(__name__)
-
-def get_results():
-    '''create connection to openaq'''
-    api = openaq.OpenAQ()
-    status, body = api.measurements(city='Los Angeles', parameter='pm25')
-    if status != 200:
-        raise ValueError('Failed to retrieve data from OpenAQ API')
-
-    # Process results into list of tuples
-    results = []
-    for measurement in body['results']:
-        date = measurement['date']['utc']
-        value = measurement['value']
-        results.append((date, value))
-
-    return results
-
-@app.route('/')
-def root():
-    '''what displays in the home route'''
-    records = Record.query.filter(Record.value >= 18).all()
-    results = [(record.datetime, record.value) for record in records]
-    return str(results)
-
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
 DB = SQLAlchemy(app)
 
-class Record(DB.Model):
-    '''create schema for datatable'''
-    id = DB.Column(DB.Integer, primary_key=True)
-    datetime = DB.Column(DB.String)
-    value = DB.Column(DB.Float, nullable=False)
 
+@app.route('/')
+def root():
+    '''what displays in the home route'''
+    records = Record.query.filter(Record.value >= 18).all()
+    results = [Record(datetime=record.datetime,
+                      value=record.value) for record in records]
+    return str(results)
 
-    def __repr__(self):
-        return f'Record(datetime={self.datetime}, value={self.value})'
 
 @app.route('/refresh')
 def refresh():
@@ -58,3 +36,30 @@ def refresh():
 
     DB.session.commit()
     return 'Data refreshed!'
+
+
+def get_results():
+    '''create connection to openaq'''
+    api = OpenAQ()
+    status, body = api.measurements(city='Los Angeles', parameter='pm25')
+    if status != 200:
+        raise ValueError('Failed to retrieve data from OpenAQ API')
+
+    # Process results into list of tuples
+    results = []
+    for measurement in body['results']:
+        date = measurement['date']['utc']
+        value = measurement['value']
+        results.append((date, value))
+
+    return results
+
+
+class Record(DB.Model):
+    '''create schema for datatable'''
+    id = DB.Column(DB.Integer, primary_key=True)
+    datetime = DB.Column(DB.String)
+    value = DB.Column(DB.Float, nullable=False)
+
+    def __repr__(self):
+        return f'Record(datetime={self.datetime}, value={self.value})'
